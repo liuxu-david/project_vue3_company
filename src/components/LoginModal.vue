0306b237156ca570c02bac3425ec99a5e05f651c
liuxu <2982359231@qq.com>
2024-08-08 14:44:42 +0800

    代码优化

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -1,63 +1,40 @@
 <script setup>
-import { ref,reactive } from "vue";
-import { handleLoginInterface } from "@/api/login.js"
-import { setItem } from "@/utils/storage.js"
-import { useRouter } from 'vue-router'
+import { ref, reactive } from "vue";
+import { handleLoginInterface } from "@/api/login.js";
+import { setItem } from "@/utils/storage.js";
+import { useRouter } from "vue-router";
 
 const router = useRouter();
-const usernameFocused = ref(false);
-const codenameFocused = ref(false);
-const passwordFocused = ref(false);
-const authcodeFocused = ref(false);
+const isForcused = reactive({
+  usernameFocused: false,
+  codenameFocused: false,
+  passwordFocused: false,
+  authcodeFocused: false,
+});
 const loginData = reactive({
   username: "",
   codename: "",
   password: "",
-  authcode: ""
+  authcode: "",
 });
 
-const handleUsernameFocus = () => {
-  usernameFocused.value = true;
+const handleFocus = (flagKey) => {
+  isForcused[flagKey] = true;
 };
-const handleUsernameBlur = () => {
-  if(loginData.username === ""){
-    usernameFocused.value = false;
-  }
-};
-const handleCodenameFocus = () => {
-  codenameFocused.value = true;
-};
-const handleCodenameBlur = () => {
-  if(loginData.codename === ""){
-    codenameFocused.value = false;
-  }
-};
-const handlePasswordFocus = () => {
-  passwordFocused.value = true;
-};
-const handlePasswordBlur = () => {
-  if(loginData.password === ""){
-    passwordFocused.value = false;
-  }
-};
-const handleAuthcodeFocus = () => {
-  authcodeFocused.value = true;
-};
-const handleAuthcodeBlur = () => {
-  if(loginData.authcode === ""){
-    authcodeFocused.value = false;
+const handleBlur = (dataKey, flagKey) => {
+  if (loginData[dataKey] === "") {
+    isForcused[flagKey] = false;
   }
 };
 
-const handleLogin = async ()=>{
+const handleLogin = async () => {
   const result = await handleLoginInterface(loginData);
   console.log(result);
-  if(Reflect.ownKeys(result).length !== 0){
+  if (Reflect.ownKeys(result).length !== 0) {
     setItem("token", result.token);
-    router.push("/home")
+    router.push("/home");
   }
-}
-
+};
 </script>
 
 <template>
@@ -71,14 +48,62 @@ const handleLogin = async ()=>{
         <h2>登录</h2>
         首次使用者? <span>点此开始使用</span>
       </div>
-      <div class="list"><div class="tip_text" :class="{'tip_text_active': usernameFocused}">IGTB平台编号/登录名称</div><input type="text" v-model="loginData.username" @focus="handleUsernameFocus"  
-      @blur="handleUsernameBlur"></div>
-      <div class="list"><div class="tip_text" :class="{'tip_text_active': codenameFocused}">使用者代号</div><input type="text" v-model="loginData.codename" @focus="handleCodenameFocus"  
-      @blur="handleCodenameBlur"></div>
-      <div class="list"><div class="tip_text" :class="{'tip_text_active': passwordFocused}">密码</div><input type="password" v-model="loginData.password" @focus="handlePasswordFocus"  
-      @blur="handlePasswordBlur"></div>
-      <div class="list"><div class="tip_text" :class="{'tip_text_active': authcodeFocused}">验证码</div><input type="text" v-model="loginData.authcode" @focus="handleAuthcodeFocus"  
-      @blur="handleAuthcodeBlur"></div>
+      <div class="list">
+        <div
+          class="tip_text"
+          :class="{ tip_text_active: isForcused.usernameFocused }"
+        >
+          IGTB平台编号/登录名称
+        </div>
+        <input
+          type="text"
+          v-model="loginData.username"
+          @focus="handleFocus('usernameFocused')"
+          @blur="handleBlur('username', 'usernameFocused')"
+        />
+      </div>
+      <div class="list">
+        <div
+          class="tip_text"
+          :class="{ tip_text_active: isForcused.codenameFocused }"
+        >
+          使用者代号
+        </div>
+        <input
+          type="text"
+          v-model="loginData.codename"
+          @focus="handleFocus('codenameFocused')"
+          @blur="handleBlur('codename', 'codenameFocused')"
+        />
+      </div>
+      <div class="list">
+        <div
+          class="tip_text"
+          :class="{ tip_text_active: isForcused.passwordFocused }"
+        >
+          密码
+        </div>
+        <input
+          type="password"
+          v-model="loginData.password"
+          @focus="handleFocus('passwordFocused')"
+          @blur="handleBlur('password', 'passwordFocused')"
+        />
+      </div>
+      <div class="list">
+        <div
+          class="tip_text"
+          :class="{ tip_text_active: isForcused.authcodeFocused }"
+        >
+          验证码
+        </div>
+        <input
+          type="text"
+          v-model="loginData.authcode"
+          @focus="handleFocus('authcodeFocused')"
+          @blur="handleBlur('authcode', 'authcodeFocused')"
+        />
+      </div>
       <div class="login_button">
         <span class="btn" @click="handleLogin">基本登录</span>
         <span class="btn">双重认证登录</span>
@@ -96,30 +121,30 @@ const handleLogin = async ()=>{
   .border {
     height: 4px;
     background-color: rgb(181, 26, 37);
-    .border_right{
+    .border_right {
       width: 100px;
       height: 4px;
       background-color: rgb(124, 135, 142);
     }
   }
-  .form{
+  .form {
     color: white;
-     padding: 0 10px;
-    .title{
+    padding: 0 10px;
+    .title {
       text-align: left;
       margin: 50px 10px 20px 0px;
-      h2{
+      h2 {
         margin-bottom: 5px;
       }
-      span{
+      span {
         font-weight: 700;
       }
     }
-    .list{
+    .list {
       height: 50px;
       position: relative;
       margin-bottom: 20px;
-      input{
+      input {
         position: absolute;
         bottom: 0;
         left: 0;
@@ -131,35 +156,35 @@ const handleLogin = async ()=>{
         color: white;
         box-sizing: border-box;
       }
-      .tip_text{
+      .tip_text {
         position: absolute;
         top: 15px;
         left: 0;
         text-align: left;
-        }
-      .tip_text_active{
+      }
+      .tip_text_active {
         top: 0;
         font-size: 12px;
         font-weight: 700;
       }
     }
-    .login_button{
+    .login_button {
       text-align: right;
       margin-top: 30px;
-      :first-child{
-          margin-right: 20px;
-        }
-    
-      .btn{
+      :first-child {
+        margin-right: 20px;
+      }
+
+      .btn {
         height: 40px;
         border: 1px solid white;
         padding: 12px 40px;
         border-radius: 5px;
         cursor: pointer;
       }
-      .btn:hover{
-          background-color: white;
-          color: black;
+      .btn:hover {
+        background-color: white;
+        color: black;
       }
     }
   }
